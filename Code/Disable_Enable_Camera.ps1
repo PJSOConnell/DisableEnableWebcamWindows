2d35dd245b4b1f78e2f992b66a437677b0c18966
PJSOConnell <pjsoconnell@gmail.com>
2020-03-23 15:14:20 +0000

    Add files via upload

---
+++ Code/Disable_Enable_Camera.ps1
@@ -0,0 +1,24 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+$camera = $null
+$camera = (Get-PnpDevice -Class "Camera" -ErrorAction SilentlyContinue)
+if($null -ne $camera){
+$camera = $camera[0]
+if ($camera.Status.ToLower() -eq "ok"){
+Write-Host "$($camera.FriendlyName)" is currently enabled.
+Write-Host The device will now be disabled.
+Disable-PnpDevice -InstanceId $camera.InstanceId -confirm:$false
+Write-Host Complete!
+}
+
+if ($camera.Status.ToLower() -eq "error"){
+Write-Host "$($camera.FriendlyName)" is currently disabled.
+Write-Host The device will now be enabled.
+Enable-PnpDevice -InstanceId $camera.InstanceId -confirm:$false
+Write-Host Complete!
+}
+}else{
+Write-Host No camera found!
+Write-Host Exiting...
+}
+
+Start-Sleep -s 5
---
+++ Code/Get_Camera_Status.ps1
@@ -0,0 +1,20 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+$camera = $null
+$camera = (Get-PnpDevice -Class "Camera" -ErrorAction SilentlyContinue)
+if($null -ne $camera){
+$camera = $camera[0]
+if ($camera.Status.ToLower() -eq "ok"){
+Write-Host "$($camera.FriendlyName)" is currently enabled.
+}
+
+elseif ($camera.Status.ToLower() -eq "error"){
+Write-Host "$($camera.FriendlyName)" is currently disabled.
+}else{
+Write-Host Current status of "$($camera.FriendlyName)" - $($camera.Status).
+}
+}else{
+Write-Host No camera found!
+Write-Host Exiting...
+}
+
+Start-Sleep -s 5
